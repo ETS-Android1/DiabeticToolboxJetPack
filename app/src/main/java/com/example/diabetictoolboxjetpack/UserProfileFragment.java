76ee64b535ad9e416ad4adf99ac6104f6f222236
kodobi <kodobi@gmail.com>
2021-01-10 22:45:26 +0100

    Create classes for data presentation. Prepare main activity for hosting Fragments.

---
+++ app/src/main/java/com/example/diabetictoolboxjetpack/UserProfileFragment.java
@@ -0,0 +1,10 @@
+package com.example.diabetictoolboxjetpack;
+
+import androidx.fragment.app.Fragment;
+
+public class UserProfileFragment extends Fragment {
+
+    public UserProfileFragment(){
+        super(R.layout.user_profile);
+    }
+}
---
+++ app/src/main/java/com/example/diabetictoolboxjetpack/UserProfileViewModel.java
@@ -0,0 +1,9 @@
+package com.example.diabetictoolboxjetpack;
+
+import androidx.lifecycle.ViewModel;
+
+public class UserProfileViewModel extends ViewModel {
+
+    //private String userID = TODO();
+    //private User user = TODO();
+}
