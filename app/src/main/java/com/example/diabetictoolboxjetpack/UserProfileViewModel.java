197db756c073b89b430483e68ddfb0eab88d5b8a
kodobi <kodobi@gmail.com>
2021-01-12 17:08:56 +0100

    Update UserProfileViewModel class.

--- app/src/main/java/com/example/diabetictoolboxjetpack/UserProfileViewModel.java
+++ app/src/main/java/com/example/diabetictoolboxjetpack/UserProfileViewModel.java
@@ -1,8 +1,30 @@
 package com.example.diabetictoolboxjetpack;
 
-import androidx.lifecycle.ViewModel;
+import android.app.Application;
 
-public class UserProfileViewModel extends ViewModel {
+import androidx.lifecycle.AndroidViewModel;
+import androidx.lifecycle.LiveData;
+
+import java.util.List;
+
+public class UserProfileViewModel extends AndroidViewModel{
+
+    private UserRepository userRepository;
+    private final LiveData<List<User>> allUsers;
+
+    public UserProfileViewModel(Application application){
+        super(application);
+        userRepository = new UserRepository(application);
+        allUsers = userRepository.getAllUsers();
+    }
+
+    LiveData<List<User>> getAllUsers(){
+        return allUsers;
+    }
+
+    public void insert(User user){
+        userRepository.insert(user);
+    }
 
     //private String userID = TODO();
     //private User user = TODO();
