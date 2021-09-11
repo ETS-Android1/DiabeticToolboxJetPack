d744ba50b2156f848eaaec30592c84b01574cd99
kodobi <kodobi@gmail.com>
2021-09-11 14:24:18 +0200

    Add user package with basic classes

--- app/src/main/java/com/kucharzyk/diabetictoolboxjetpack/logging_and_registration/LoggingActivity.java
+++ app/src/main/java/com/kucharzyk/diabetictoolboxjetpack/logging_and_registration/LoggingActivity.java
@@ -5,24 +5,38 @@ import android.os.Bundle;
 import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
 import com.kucharzyk.diabetictoolboxjetpack.R;
 import com.kucharzyk.diabetictoolboxjetpack.ui_and_data.MainActivity;
+import com.kucharzyk.diabetictoolboxjetpack.ui_and_data.user.UserViewModel;
 
 
 public class LoggingActivity extends AppCompatActivity {
     private static final String TAG = "LoggingActivity";
 
+    private UserViewModel userViewModel;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logging);
+
+        userViewModel = new ViewModelProvider(this).get(UserViewModel.class);
+
     }
 
-    /** Called when the user taps the Send button */
-    public void changeActivity(View view) {
+
+
+    /** Called when the user taps the Login button */
+    public void login(View view) {
         Intent myIntent = new Intent(this, MainActivity.class);
         startActivity(myIntent);
+    }
+
+    /** Called when the user taps the Register button */
+    public void register(View view){
+        //TODO
     }
 
 
---
+++ app/src/main/java/com/kucharzyk/diabetictoolboxjetpack/ui_and_data/user/UserViewModel.java
@@ -0,0 +1,8 @@
+package com.kucharzyk.diabetictoolboxjetpack.ui_and_data.user;
+
+import androidx.lifecycle.ViewModel;
+
+public class UserViewModel extends ViewModel {
+    private static final String TAG = "UserViewModel";
+
+}
